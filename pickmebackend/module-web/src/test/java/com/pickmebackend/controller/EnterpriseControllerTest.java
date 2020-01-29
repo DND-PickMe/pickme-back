dc97a3ef7257a18bf7d7a248464cc0b74e40766e
yks095 <rltjr219@naver.com>
2020-01-29 20:45:11 +0900

    Add Enterprise CRUD
      - issue 대로 구현하였음

--- pickmebackend/module-web/src/test/java/com/pickmebackend/controller/EnterpriseControllerTest.java
+++ pickmebackend/module-web/src/test/java/com/pickmebackend/controller/EnterpriseControllerTest.java
@@ -6,7 +6,8 @@ import com.pickmebackend.domain.Enterprise;
 import com.pickmebackend.domain.dto.EnterpriseDto;
 import com.pickmebackend.properties.AppProperties;
 import com.pickmebackend.repository.EnterpriseRepository;
-import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -23,6 +24,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import java.time.LocalDateTime;
 import java.util.UUID;
 import java.util.stream.Stream;
 import static com.pickmebackend.error.ErrorMessageConstant.DUPLICATEDUSER;
@@ -58,22 +60,61 @@ class EnterpriseControllerTest {
     @Autowired
     JwtProvider jwtProvider;
 
+    private String enterpriseURL = "/api/enterprises/";
+
     private String jwt;
 
-    // TODO POST 정상적으로 기업담담자 생성(Status : 201 Created, Body : 생성된 기업담당자 객체)
+    @BeforeEach
+    void setUp()    {
+        enterpriseRepository.deleteAll();
+    }
+
+    // TODO GET 요청 시 정상적으로 모든 기업 담당자 불러오기(Status : 200, Body : List<Enterprise> : 미정)
+
+    @Test
+    @DisplayName("정상적으로 한명의 기업담당자 불러오기")
+    void load_enterprise() throws Exception {
+        Enterprise enterprise = createEnterprise();
+//        jwt = jwtProvider.generateToken(enterprise);
+
+        this.mockMvc.perform(get(enterpriseURL + "{enterpriseId}", enterprise.getId())
+                .accept(MediaTypes.HAL_JSON_VALUE)
+//                        .header(HttpHeaders.AUTHORIZATION, jwt)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+        ;
+    }
+
+    @Test
+    @DisplayName("저장되어 있지 않은 기업담당자 불러 오기")
+    void load_non_enterprise() throws Exception {
+        Enterprise enterprise = createEnterprise();
+//        jwt = jwtProvider.generateToken(enterprise);
+
+        this.mockMvc.perform(get(enterpriseURL + "{enterpriseId}", -1)
+                .accept(MediaTypes.HAL_JSON_VALUE)
+//                        .header(HttpHeaders.AUTHORIZATION, jwt)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("message", is(USERNOTFOUND)))
+        ;
+    }
+
     @Test
     @DisplayName("정상적으로 기업담담자 생성")
     void save_enterprise() throws Exception {
         EnterpriseDto enterpriseDto = createEnterpriseDto();
 
-        ResultActions actions = this.mockMvc.perform(post("/api/enterprises")
+        ResultActions actions = this.mockMvc.perform(post(enterpriseURL)
                 .accept(MediaTypes.HAL_JSON_VALUE)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(enterpriseDto)))
                 .andDo(print())
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("id").exists())
-                .andExpect(jsonPath("email").value(appProperties.getTestCeoName()))
+                .andExpect(jsonPath("email").value(appProperties.getTestEmail()))
                 .andExpect(jsonPath("password").doesNotExist())
                 .andExpect(jsonPath("registrationNumber").value(appProperties.getTestRegistrationNumber()))
                 .andExpect(jsonPath("name").value(appProperties.getTestName()))
@@ -89,27 +130,25 @@ class EnterpriseControllerTest {
         assertEquals(enterprise.getName(), appProperties.getTestName());
         assertEquals(enterprise.getAddress(), appProperties.getTestAddress());
         assertEquals(enterprise.getCeoName(), appProperties.getTestCeoName());
+        assertNotNull(enterprise.getCreatedAt());
     }
 
-    // TODO POST 저장 시 동일한 email을 가진 유저가 존재할 경우(Status : 400 Bad Request, Body : ErrorMessage 객체)
     @Test
     @DisplayName("기업 담당자 저장 시 동일한 email을 가진 유저가 존재할 경우 Bad Request 반환")
     void check_duplicated_enterprise() throws Exception {
         Enterprise enterprise = createEnterprise();
         EnterpriseDto enterpriseDto = createEnterpriseDto();
 
-        this.mockMvc.perform(post("/api/enterprises")
+        this.mockMvc.perform(post(enterpriseURL)
                         .accept(MediaTypes.HAL_JSON_VALUE)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(enterpriseDto)))
                 .andDo(print())
                 .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("message", Matchers.is(DUPLICATEDUSER)))
+                .andExpect(jsonPath("message", is(DUPLICATEDUSER)))
         ;
     }
 
-
-    // TODO POST 저장 시 필수로 들어와야할 필드값 중 하나라도 들어오지 않을 경우 - 빈 문자열 (Status : 400 Bad Request, Body : Errors 객체)
     @ParameterizedTest
     @DisplayName("기업 담당자 저장시 하나의 필드라도 공백이 들어올 경우 Bad Request 반환")
     @MethodSource("streamForEmptyStringCheck")
@@ -123,7 +162,7 @@ class EnterpriseControllerTest {
                 .ceoName(ceoName)
                 .build();
 
-        this.mockMvc.perform(post("/api/enterprises")
+        this.mockMvc.perform(post(enterpriseURL)
                         .accept(MediaTypes.HAL_JSON_VALUE)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(enterpriseDto)))
@@ -136,9 +175,6 @@ class EnterpriseControllerTest {
         ;
     }
 
-
-
-    // TODO POST 저장 시 필수로 들어와야할 필드값 중 하나라도 들어오지 않을 경우 - null (Status : 400 Bad Request, Body : Errors 객체)
     @ParameterizedTest
     @DisplayName("기업 담당자 저장시 하나의 필드라도 null이 들어올 경우 Bad Request 반환")
     @MethodSource("streamForNullCheck")
@@ -152,7 +188,7 @@ class EnterpriseControllerTest {
                 .ceoName(ceoName)
                 .build();
 
-        this.mockMvc.perform(post("/api/enterprises")
+        this.mockMvc.perform(post(enterpriseURL)
                 .accept(MediaTypes.HAL_JSON_VALUE)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(enterpriseDto)))
@@ -165,25 +201,24 @@ class EnterpriseControllerTest {
         ;
     }
 
-
-    // TODO PUT 정상적으로 수정(Status : 200 OK, Body : 수정된 기업담당자 객체)
     @Test
     @DisplayName("정상적으로 기업 담당자 수정")
     void update_enterprise() throws Exception {
         Enterprise enterprise = createEnterprise();
 //        jwt = jwtProvider.generateToken(enterprise);
+
+        enterprise.setEmail("newEmail@email.com");
+        enterprise.setPassword("newPassword");
+        enterprise.setRegistrationNumber("newRegistrationNumber");
+        enterprise.setName("newName");
+        enterprise.setAddress("newAddress");
+        enterprise.setCeoName("newCeoName");
+
         EnterpriseDto enterpriseDto = modelMapper.map(enterprise, EnterpriseDto.class);
 
-        enterpriseDto.setEmail("newEmail@email.com");
-        enterpriseDto.setPassword("newPassword");
-        enterpriseDto.setRegistrationNumber("newRegistrationNumber");
-        enterpriseDto.setName("newName");
-        enterpriseDto.setAddress("newAddress");
-        enterpriseDto.setCeoName("newCeoName");
-
-        this.mockMvc.perform(put("/api/enterprises/", enterprise.getId())
+        this.mockMvc.perform(put(enterpriseURL + "{enterpriseId}", enterprise.getId())
                         .accept(MediaTypes.HAL_JSON_VALUE)
-                        .header(HttpHeaders.AUTHORIZATION, jwt)
+//                        .header(HttpHeaders.AUTHORIZATION, jwt)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(enterpriseDto)))
                 .andDo(print())
@@ -199,9 +234,6 @@ class EnterpriseControllerTest {
         ;
     }
 
-
-
-    // TODO PUT 데이터베이스에 저장되어 있지 않은 기업담당자 수정 요청(Status : 400 Bad Request, Body : ErrorMessage 객체)
     @Test
     @DisplayName("수정시 존재 하지 않는 기업 담당자 수정 할 경우 Bad Request 반환")
     void update_non_enterprise() throws Exception {
@@ -216,9 +248,9 @@ class EnterpriseControllerTest {
         enterpriseDto.setAddress("newAddress");
         enterpriseDto.setCeoName("newCeoName");
 
-        this.mockMvc.perform(put("/api/enterprises/", UUID.randomUUID().toString())
+        this.mockMvc.perform(put(enterpriseURL + "{enterpriseId}", -1)
                         .accept(MediaTypes.HAL_JSON_VALUE)
-                        .header(HttpHeaders.AUTHORIZATION, jwt)
+//                        .header(HttpHeaders.AUTHORIZATION, jwt)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(enterpriseDto)))
                 .andDo(print())
@@ -227,26 +259,25 @@ class EnterpriseControllerTest {
         ;
     }
 
-
-    // TODO PUT 수정 시 필수로 들어와야할 필드값 중 하나라도 들어오지 않을 경우 - 빈 문자열 (Status : 400 Bad Request, Body : Errors 객체)
     @ParameterizedTest
     @DisplayName("기업 담당자 수정시 하나의 필드라도 공백이 들어올 경우 Bad Request 반환")
     @MethodSource("streamForEmptyStringCheck")
     void check_update_empty_string_enterprise(String email, String password, String registrationNumber, String name, String address, String ceoName) throws Exception {
         Enterprise enterprise = createEnterprise();
 //        jwt = jwtProvider.generateToken(enterprise);
+
+        enterprise.setEmail(email);
+        enterprise.setPassword(password);
+        enterprise.setRegistrationNumber(registrationNumber);
+        enterprise.setName(name);
+        enterprise.setAddress(address);
+        enterprise.setCeoName(ceoName);
+
         EnterpriseDto enterpriseDto = modelMapper.map(enterprise, EnterpriseDto.class);
 
-        enterpriseDto.setEmail(email);
-        enterpriseDto.setPassword(password);
-        enterpriseDto.setRegistrationNumber(registrationNumber);
-        enterpriseDto.setName(name);
-        enterpriseDto.setAddress(address);
-        enterpriseDto.setCeoName(ceoName);
-
-        this.mockMvc.perform(put("/api/enterprises/", enterprise.getId())
+        this.mockMvc.perform(put(enterpriseURL + "{enterpriseId}", enterprise.getId())
                         .accept(MediaTypes.HAL_JSON_VALUE)
-                        .header(HttpHeaders.AUTHORIZATION, jwt)
+//                        .header(HttpHeaders.AUTHORIZATION, jwt)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(enterpriseDto)))
                 .andDo(print())
@@ -258,26 +289,25 @@ class EnterpriseControllerTest {
         ;
     }
 
-
-    // TODO PUT 수정 시 필수로 들어와야할 필드값 중 하나라도 들어오지 않을 경우 - null (Status : 400 Bad Request, Body : Errors 객체)
     @ParameterizedTest
     @DisplayName("기업 담당자 수정시 하나의 필드라도 null이 들어올 경우 Bad Request 반환")
     @MethodSource("streamForNullCheck")
     void check_update_null_enterprise(String email, String password, String registrationNumber, String name, String address, String ceoName) throws Exception {
         Enterprise enterprise = createEnterprise();
 //        jwt = jwtProvider.generateToken(enterprise);
+
+        enterprise.setEmail(email);
+        enterprise.setPassword(password);
+        enterprise.setRegistrationNumber(registrationNumber);
+        enterprise.setName(name);
+        enterprise.setAddress(address);
+        enterprise.setCeoName(ceoName);
+
         EnterpriseDto enterpriseDto = modelMapper.map(enterprise, EnterpriseDto.class);
 
-        enterpriseDto.setEmail(email);
-        enterpriseDto.setPassword(password);
-        enterpriseDto.setRegistrationNumber(registrationNumber);
-        enterpriseDto.setName(name);
-        enterpriseDto.setAddress(address);
-        enterpriseDto.setCeoName(ceoName);
-
-        this.mockMvc.perform(put("/api/enterprises/", enterprise.getId())
+        this.mockMvc.perform(put(enterpriseURL + "{enterpriseId}", enterprise.getId())
                 .accept(MediaTypes.HAL_JSON_VALUE)
-                .header(HttpHeaders.AUTHORIZATION, jwt)
+//                .header(HttpHeaders.AUTHORIZATION, jwt)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(enterpriseDto)))
                 .andDo(print())
@@ -289,80 +319,32 @@ class EnterpriseControllerTest {
         ;
     }
 
-    // TODO GET 요청 시 정상적으로 한명의 기업담당자 불러오기(Status : 200, Body : Enterprise 객체)
-    @Test
-    @DisplayName("정상적으로 한명의 기업담당자 불러오기")
-    void load_enterprise() throws Exception {
-        Enterprise enterprise = createEnterprise();
-//        jwt = jwtProvider.generateToken(enterprise);
-
-        this.mockMvc.perform(get("/api/enterprises/", enterprise.getId())
-                        .accept(MediaTypes.HAL_JSON_VALUE)
-                        .header(HttpHeaders.AUTHORIZATION, jwt)
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andDo(print())
-                .andExpect(status().isOk())
-        ;
-    }
-
-
-    // TODO GET 요청 시 저장되어 있지 않은 기업담당자 불러 오기(Status : 400, Body : ErrorMessage 객체)
-    @Test
-    @DisplayName("저장되어 있지 않은 기업담당자 불러 오기")
-    void load_non_enterprise() throws Exception {
-        Enterprise enterprise = createEnterprise();
-//        jwt = jwtProvider.generateToken(enterprise);
-
-        this.mockMvc.perform(get("/api/enterprises/", UUID.randomUUID().toString())
-                        .accept(MediaTypes.HAL_JSON_VALUE)
-                        .header(HttpHeaders.AUTHORIZATION, jwt)
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andDo(print())
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("message", is(USERNOTFOUND)))
-        ;
-    }
-
-
-    // TODO GET 요청 시 정상적으로 모든 기업 담당자 불러오기(Status : 200, Body : List<Enterprise> : 미정)
-
-
-
-    // TODO 정상적으로 삭제(Status : 200 OK)
     @Test
     @DisplayName("정상적으로 기업 담당자 삭제")
     void delete_enterprise() throws Exception {
         Enterprise enterprise = createEnterprise();
 //        jwt = jwtProvider.generateToken(enterprise);
 
-        this.mockMvc.perform(delete("/api/enterprises/", enterprise.getId())
-                        .accept(MediaTypes.HAL_JSON_VALUE)
-                        .header(HttpHeaders.AUTHORIZATION, jwt)
-                        .contentType(MediaType.APPLICATION_JSON))
+        this.mockMvc.perform(delete(enterpriseURL + "{enterpriseId}", enterprise.getId()))
+//                        .header(HttpHeaders.AUTHORIZATION, jwt)
                 .andDo(print())
                 .andExpect(status().isOk())
         ;
     }
 
-
-    // TODO 데이터베이스에 저장되어 있지 않은 기업담당자 삭제 요청(Status : 400 Bad Request, Body : ErrorMessage 객체)
     @Test
     @DisplayName("데이터베이스에 저장되어 있지 않은 기업담당자 삭제 요청시 Bad Request 반환")
     void delete_non_enterprise() throws Exception {
         Enterprise enterprise = createEnterprise();
 //        jwt = jwtProvider.generateToken(enterprise);
 
-        this.mockMvc.perform(delete("/api/enterprises/", UUID.randomUUID().toString())
-                        .accept(MediaTypes.HAL_JSON_VALUE)
-                        .header(HttpHeaders.AUTHORIZATION, jwt)
-                        .contentType(MediaType.APPLICATION_JSON))
+        this.mockMvc.perform(delete(enterpriseURL + "{enterpriseId}", -1))
+//                        .header(HttpHeaders.AUTHORIZATION, jwt))
                 .andDo(print())
                 .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("message", is(USERNOTFOUND)))
         ;
     }
-
-
 
     private EnterpriseDto createEnterpriseDto() {
         return EnterpriseDto.builder()
@@ -375,8 +357,10 @@ class EnterpriseControllerTest {
                 .build();
     }
 
-    private Enterprise createEnterprise()   {
+    private Enterprise createEnterprise() {
         Enterprise enterprise = modelMapper.map(createEnterpriseDto(), Enterprise.class);
+        enterprise.setCreatedAt(LocalDateTime.now());
+
         return enterpriseRepository.save(enterprise);
     }
 
