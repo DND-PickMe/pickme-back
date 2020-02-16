f8798b5079630d917d9238235ff055fe9583d154
mkshin96 <tlsanrhs96@gmail.com>
2020-02-16 20:11:14 +0900

    Refactoring gradle
    배포 준비

--- pickmebackend/module-common/src/test/java/com/pickmebackend/properties/JwtConstantsTest.java
+++ pickmebackend/module-common/src/test/java/com/pickmebackend/properties/JwtConstantsTest.java
@@ -1,14 +1,10 @@
 package com.pickmebackend.properties;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+
 import static com.pickmebackend.properties.JwtConstants.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest
-@ExtendWith(SpringExtension.class)
 class JwtConstantsTest {
 
     @Test
