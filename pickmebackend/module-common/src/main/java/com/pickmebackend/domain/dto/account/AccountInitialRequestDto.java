8ebb755eaf602dd0432563b68a7411643d38191a
mkshin96 <tlsanrhs96@gmail.com>
2020-02-28 22:27:08 +0900

    Refactoring Error Message 수정, 조회수 에러 수정

--- pickmebackend/module-common/src/main/java/com/pickmebackend/domain/dto/account/AccountInitialRequestDto.java
+++ pickmebackend/module-common/src/main/java/com/pickmebackend/domain/dto/account/AccountInitialRequestDto.java
@@ -11,13 +11,13 @@ import javax.validation.constraints.Size;
 @NoArgsConstructor @AllArgsConstructor @Builder
 public class AccountInitialRequestDto {
 
-    @Email @NotBlank
+    @Email(message = "이메일 형식이 아닙니다.") @NotBlank(message = "이메일은 반드시 입력되어야 합니다.")
     private String email;
 
-    @NotBlank @Size(min = 8, max = 20)
+    @NotBlank(message = "비밀번호는 반드시 입력되어야 합니다.") @Size(min = 8, max = 20, message = "비밀번호는 8자 이상 20자 미만이어야 합니다.")
     private String password;
 
-    @NotBlank
+    @NotBlank(message = "이름은 반드시 입력되어야 합니다.")
     private String nickName;
 
     @Nullable
