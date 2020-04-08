bc5f7b1e3d86731e040628d6ca07b089b32dc759
mkshin96 <tlsanrhs96@gmail.com>
2020-04-08 19:29:17 +0900

    modified error handler

--- pickmebackend/module-web/src/main/java/com/pickmebackend/controller/ErrorController.java
+++ pickmebackend/module-web/src/main/java/com/pickmebackend/controller/ErrorController.java
@@ -1,14 +1,16 @@
 package com.pickmebackend.controller;
 
 import com.pickmebackend.common.ErrorsFormatter;
-import com.pickmebackend.error.ErrorMessage;
 import com.pickmebackend.exception.CodeNotExist;
 import com.pickmebackend.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import static com.pickmebackend.error.ErrorMessage.USER_NOT_FOUND;
 
 @RestControllerAdvice
 @RequiredArgsConstructor
@@ -17,12 +19,14 @@ public class ErrorController {
     private final ErrorsFormatter errorsFormatter;
 
     @ExceptionHandler(CodeNotExist.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<?> codeNotExist() {
-        return new ResponseEntity<>(errorsFormatter.badRequest(ErrorMessage.USER_NOT_FOUND.getValue()), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(errorsFormatter.badRequest(USER_NOT_FOUND.getValue()), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(UserNotFoundException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<?> userNotFound() {
-        return new ResponseEntity<>(errorsFormatter.badRequest(ErrorMessage.USER_NOT_FOUND.getValue()), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(errorsFormatter.badRequest(USER_NOT_FOUND.getValue()), HttpStatus.BAD_REQUEST);
     }
 }
